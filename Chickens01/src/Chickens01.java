7677213575fd0c1df8694781361bf0499a784e25
ALUNO <ALUNO@LAB04-PC19>
2024-06-04 19:19:07 -0300

    atividades feitas em sala de aula

---
+++ Chickens01/src/Chickens01.java
@@ -0,0 +1,9 @@
+public class Chickens01 {
+    public static void main(String[] args) {
+        //Put yout code here
+        int eggPerChicken = 0;
+        int chickenCount = 0;
+        int totalEggs = 0;
+        System.out.println(totalEggs+eggPerChicken+chickenCount);
+    }   
+}
